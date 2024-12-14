98ace569dee621b13ee547d46e772588ada2b723
Lorena GONGANG <lorenagongang@MacBook-Air-de-Lorena.local>
2024-12-14 23:57:56 +0100

    remove comments and change source

--- transformation/dbt_transformation/macros/dynamic_models.sql
+++ transformation/dbt_transformation/macros/dynamic_models.sql
@@ -1,6 +1,6 @@
 {% macro dynamic_model(table_name) %}
 {% set json_keys = get_json_keys(table_name) %}
-{% set dest = '{{ source ("airbyte_internal", "' ~ table_name ~ '") }}' %}
+{% set dest = '{{ source ("healthcare", "' ~ table_name ~ '") }}' %}
 
 {% set sql %}
 {{ config(materialized='table') }}
@@ -10,11 +10,9 @@ WITH parsed_data AS (
         {% for key in json_keys %}
             {% set column_type = get_column_type(table_name, key) %}
             {% if column_type == 'DateTime' %}
-                --parseDateTime64BestEffort(JSONExtract(_airbyte_data, '{{ key }}', 'String')) AS {{ key }}
                 parseDateTime64BestEffort(JSON_EXTRACT_PATH_TEXT(JSON_SERIALIZE(_airbyte_data),  '{{ key }}')) AS {{ key }}
             {% else %}
                 JSON_EXTRACT_PATH_TEXT(JSON_SERIALIZE(_airbyte_data), '{{ key }}') AS {{ key }}
-               -- JSONExtract(_airbyte_data, '{{ key }}', '{{column_type}}') AS {{ key }}
             {% endif %}
             {% if not loop.last %},{% endif %}
         {% endfor %}

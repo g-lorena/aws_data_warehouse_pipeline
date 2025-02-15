45641a2a23ec499b0153053a960ee1f62f0c5214
Lorena GONGANG <lorenagongang@MacBook-Air-de-Lorena.local>
2025-02-15 13:39:58 +0100

    change github workflow

--- Infra/modules/vpc/main.tf
+++ Infra/modules/vpc/main.tf
@@ -407,7 +407,8 @@ resource "aws_security_group" "airflow_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["93.21.130.146/32"]
+    #cidr_blocks = ["0.0.0.0/0"]
+    security_groups = [ aws_security_group.bastion_sg.id ]
   }
 
   ingress {
